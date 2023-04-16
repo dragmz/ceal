34726f47fe0449e6f0c3f20877e37d3041654fbc
Ben Guidarelli <ben.guidarelli@gmail.com>
2023-04-16 11:12:23 -0400

    move antlr jar/license and grammar file to parser directory

--- parser/generate.go
+++ parser/generate.go
@@ -1,3 +1,3 @@
 package parser
 
-//go:generate java -jar ../antlr-4.12.0-complete.jar -Dlanguage=Go -package parser -visitor ../C.g4
+//go:generate java -jar antlr-4.12.0-complete.jar -Dlanguage=Go -package parser -visitor C.g4
