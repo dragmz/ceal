c83b443972430f487a8bbbf534c287de553a0abb
Marcin Zawiejski <dragmz@gmail.com>
2023-04-19 22:53:21 +0200

    subscript dot example

--- examples/subscript.cpp
+++ examples/subscript.cpp
@@ -1,7 +1,16 @@
 #include "avm.hpp"
 
+struct test_t
+{
+    bytes data;
+};
+
 uint64 avm_main()
 {
     const bytes sender = avm_txn.Sender;
-    return sender[2];
+
+    test_t test;
+    test.data = sender;
+
+    return sender[2] + test.data[3];
 }
