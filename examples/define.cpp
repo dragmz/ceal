f5817a0c31133288d66d7a306ef6490c96bb4665
Marcin Zawiejski <dragmz@gmail.com>
2023-05-11 21:47:45 +0200

    simplified define example as it's now separate project (dragmz/cpre) where preprocessor tests are implemented

--- examples/define.cpp
+++ examples/define.cpp
@@ -1,36 +1,8 @@
 #include "avm.hpp"
 
-#if 1
-
 #define VALUE 123
-#define TEST VALUE
-#define CONST
-
-#ifdef CONST
-#if TEST
-const uint64 ConstValue = TEST;
-#endif
-#endif
-
-#endif
 
 uint64 avm_main()
 {
-#if 1
-#ifdef ZERO
-    return 0;
-#endif
-#endif
-
-#if 0
-#if TEST
-return 1;
-#endif
-#endif
-
-#if TEST
-#ifdef CONST
-    return ConstValue;
-#endif
-#endif
+    return VALUE;
 }
